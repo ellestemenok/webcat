c5d00ea206cf4523ff42c3a2779e8fd0fb633da8
ellestemenok <145902877+ellestemenok@users.noreply.github.com>
2023-10-23 12:04:48 +0300

    Add files via upload

--- js/mouseenter_changingpic.js
+++ js/mouseenter_changingpic.js
@@ -2,21 +2,28 @@ const breedsTable = document.querySelector(".breeds-table");
 const breedcatImage = document.getElementById("breedcat-image");
 
 // Обработчик события при наведении на строку таблицы
-breedsTable.addEventListener("mouseenter", function (event) {
-    // Проверяем, что событие произошло над строкой (TR)
-    if (event.target.tagName === "tr") {
-        // Получаем ID строки
-        const rowId = event.target.id;
+breedsTable.addEventListener("mouseover", function (event) {
 
-        // Обновляем src изображения в breedcat-image-container в зависимости от ID строки
-        switch (rowId) {
-            case "abyssian-cat-row":
-                breedcatImage.src = "img/Abyssinian-Cat.webp";
-                break;
-            case "arabic-mau-row":
-                breedcatImage.src = "img/Egyptian-Mau.jpg";
-                break;
-            // Добавьте дополнительные случаи для других пород, если необходимо
+    let target = event.target;
+    // Проверяем, что событие произошло над строкой (TR)
+    while (target != this){
+
+        if (target.tagName === "TR") {
+        
+            // Получаем ID строки
+            const rowId = target.id;
+           
+            // Обновляем src изображения в breedcat-image-container в зависимости от ID строки
+            switch (rowId) {
+                case "abyssian-cat-row":
+                    breedcatImage.src = "img/Abyssinian-Cat.webp";
+                    break;
+                case "arabic-mau-row":
+                    breedcatImage.src = "img/Egyptian-Mau.jpg";
+                    break;
+                // Добавьте дополнительные случаи для других пород, если необходимо
+            }
         }
+        target = target.parentNode;
     }
 });
